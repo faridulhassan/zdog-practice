c54dd9560bd44ebfcc408ddff0ae53a8f3188444
faridulhassan <faridul940@gmail.com>
2019-10-06 11:33:07 +0600

    m

--- js/script.js
+++ js/script.js
@@ -2,18 +2,37 @@ var illo = new Zdog.Illustration({
   element: '#canvas',
   dragRotate: true,
   zoom: 1,
+  // centered: false,
 });
 var circle = new Zdog.Ellipse({
   addTo: illo,
-  stroke: 20,
-  diameter: 80,
+  stroke: 5,
+  diameter: 40,
   color: '#ff47ff',
+  translate: {z: 40},
   // stroke: false,
   // fill: true,
-  translate: {x:50}
+  // translate: {x: 160, y: 160}
 });
+circle.copy({
+  color: '#4170ee',
+  translate: {z: -100}
+});
+var rect = new Zdog.Rect({
+  addTo: illo,
+  translate: {z: -40},
+  width: 120,
+  height: 80,
+  stroke: 7,
+  color: 'red',
+  // fill: true
+});
+
 function animate() {
+  // illo.rotate.x += 0.02;
+  illo.rotate.y += 0.02;
   illo.updateRenderGraph();
   requestAnimationFrame(animate);
 }
+
 animate();
